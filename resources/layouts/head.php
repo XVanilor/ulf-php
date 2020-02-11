8128e4f8315b32a90200d5a53824215c7c002094
Vanilor <dev@vanilor.info>
2020-02-11 13:21:57 +0100

    Home page reworking

--- resources/layouts/head.php
+++ resources/layouts/head.php
@@ -1,17 +1,16 @@
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <title><?= $config['app_name']; ?><?= (isset($datas["page_name"])) ? " - ".$datas['page_name'] : "" ?></title>
+    <title>UL-F Home page</title>
 
-    <!-- Custom fonts-->
-    <link rel="stylesheet" href="<?= assets('vendor/fontawesome-free/css/all.min.css'); ?>" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> <!--- TODO : Donner un accès local à la font -->
-
-    <!-- Custom styles-->
-    <link rel="stylesheet" href="<?= assets('css/sb-admin-2.min.css'); ?>" />
-    <link rel="stylesheet" href="<?= assets('vendor/datatables/dataTables.bootstrap4.min.css'); ?>" />
+    <!-- Bootstrap Core CSS -->
+    <link href="<?= assets("css/bootstrap.min.css"); ?>" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?= assets("css/custom.min.css"); ?>" rel="stylesheet">
 
 </head>
