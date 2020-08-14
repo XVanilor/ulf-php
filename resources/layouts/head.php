b3f981610b252a82cfadae391a07f9e7a04de4d1
Vanilor <dev@vanilor.info>
2020-08-14 22:34:22 +0200

    Language spelling mistake correction

--- resources/layouts/head.php
+++ resources/layouts/head.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title><?= $config['app_name']; ?><?= (isset($datas["page_name"])) ? " - ".$datas['page_name'] : "" ?></title>
+    <title><?= $config['app_name']; ?><?= (isset($data["page_name"])) ? " - ".$data['page_name'] : "" ?></title>
 
     <link rel="stylesheet" type="text/css" href="<?= assets("css/bootstrap.min.css"); ?>" />
     <link rel="stylesheet" type="text/css" href="<?= assets("css/custom.min.css"); ?>" />
