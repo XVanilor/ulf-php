7b3a196207ac0e6893982dad49931446b4412a0c
Vanilor <services+gitlab@vanilor.net>
2023-10-15 11:59:37 +0200

    Updated to PHP 8

--- config/middlewares.php
+++ config/middlewares.php
@@ -4,7 +4,6 @@ return [
 
     "middlewares" => [
 
-        "admin" => \App\Modules\Authentication\Middlewares\AdminMiddleware::class,
         "auth" => \App\Modules\Authentication\Middlewares\AuthMiddleware::class
 
     ]
